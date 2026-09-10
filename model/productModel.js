98daec20bf88465d4d4b6484a21c5c651a901bac
arinzeorji4 <arinzeorji4@gmail.com>
2026-09-10 19:48:00 +0100

    added products route

---
+++ model/productModel.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name:{type: String, required: true},
+    description:{type: String, required: true},
+    price:{type: Number, required: true},
+    category:{type: String, required: true},
+    stock:{type: String, required: true},
+    quantity:{type:String, required: true},
+    image:{type: String, required: true},
+    
+}, {timestamp: true});
+
+const productModel = mongoose.model("Product", productSchema);
+
+module.exports = productModel;
